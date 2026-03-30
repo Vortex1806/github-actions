5acf0ca306c1279f9b46f588df157564d363ab23
Shubh Vora <78277252+Vortex1806@users.noreply.github.com>
2026-03-30 22:23:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flast, render_template
 app = Flast(__name__)
 
+
 @app.route('/')
 def hello_world():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/health')
 def health():
-  return 'Server is up and running'
+    return 'Server is up and running'
